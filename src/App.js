21c7f68ec7015b3e58659a3142e968359c18b61b
Nick-Maldonado <nickmaldonado94@gmail.com>
2018-04-17 06:32:26 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+var config = {
+  apiKey: "AIzaSyBaI2ws1B52fUbQDCLvEyAjtPW8RObNn7k",
+  authDomain: "bloc-chat-react-c0db3.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-c0db3.firebaseio.com",
+  projectId: "bloc-chat-react-c0db3",
+  storageBucket: "bloc-chat-react-c0db3.appspot.com",
+  messagingSenderId: "811868769665"
+};
+firebase.initializeApp(config);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <RoomList />
+      </div>
+    );
+  }
+}
+
+export default App;
