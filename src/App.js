0e01ca103ed6b33ee4274c2a6c63e0276c93117b
Nick-Maldonado <nickmaldonado94@gmail.com>
2018-04-17 06:48:11 -0500

    Receiving the following error:  TypeError: instance.render is not a function

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <RoomList />
+        <RoomList 
+        firebase={firebase}
+        />
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -9,13 +9,14 @@ class RoomList extends Component {
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
-
-    componentDidMount() {
-      this.roomsRef.on('child_added', snapshot => {
-        this.setState({ rooms: this.state.rooms.concat( snapshot.val() ) });
-      });
-    }
-	}
+  }
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) })
+    });
+  }
 }
 
 export default RoomList;
