5714be7e37e593f6614e1f8fd986680eccfc18bc
Nick-Maldonado <nickmaldonado94@gmail.com>
2018-04-21 17:52:50 -0500

    one bug left to fix

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -11,35 +11,33 @@ class MessageList extends Component {
     this.messagesRef = this.props.firebase.database().ref('messages');
   }  
 
-  componentDidMount() {
-    console.log(this.props.activeRoom) // this doesn't work?
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.activeRoom === this.props.activeRoom) { return };
+    this.setState({ messages: [] });
     this.messagesRef.on('child_added', snapshot => {
       const message = snapshot.val();
-      // console.log(message.roomId);
-      if (message.roomId.toString() === this.props.activeRoom.toString()) {
-        console.log(message)
-        // this.setState({ messages: this.state.messages.concat( message ) });
+      if (message.roomId.toString() === nextProps.activeRoom.toString()) {
+        console.log(message.roomId.toString())
+        this.setState({ messages: this.state.messages.concat( message ) });
       }
     });
   }
 
 
   render() {
-    // const messages = ( 
-    //   this.state.messages.map( (message, index) =>
-    //     <div className="message">
-    //       <div className="message-username">{message.username}</div>
-    //       <div className="message-sentAt">{message.sentAt}</div>
-    //       <div className="message-content">{message.content}</div>
-    //     </div>
-    //   )
-    // );
+    const messages = ( 
+      this.state.messages.map( (message, index) =>
+        <div className="message" key={`message ${index}`}>
+          <div className="message-username">{message.username}</div>
+          <div className="message-sentAt">{message.sentAt}</div>
+          <div className="message-content">{message.content}</div>
+        </div>
+      )
+    );
 
     return(
       <section className="chat-messages">
-        {this.state.messages}
-        {this.props.activeRoom}
-        {/* {messages} */}
+        {messages}
       </section>
     );
   }
